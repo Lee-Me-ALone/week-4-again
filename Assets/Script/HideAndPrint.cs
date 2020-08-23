c366e9fc107142322037a583a56aadfb8b94dac6
lee_me_alone <13877713@student.uts.edu.au>
2020-08-24 02:57:22 +1000

    100 Percent

--- Assets/Script/HideAndPrint.cs
+++ Assets/Script/HideAndPrint.cs
@@ -7,16 +7,24 @@ public class HideAndPrint : MonoBehaviour
 
     int i = 0;
     public Renderer rend;
+    int x;
+    //int x = Random.Range(200, 250);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameObject.CompareTag("Red") &&  i == 100)
+            gameObject.SetActive(false);
+
+        if (gameObject.CompareTag("Blue") && i == x)
+            rend.enabled = false;
+
         Debug.Log(gameObject.name + ":" + i);
         i++;
     }
--- Assets/Script/LoadAssets.cs
+++ Assets/Script/LoadAssets.cs
@@ -7,8 +7,8 @@ public class LoadAssets : MonoBehaviour
 
     public GameObject redObj;
 
-    //[SerializeField]
-    public GameObject blueObj;
+    [SerializeField]
+    private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
